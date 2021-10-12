ddd8de915ea49b249adb4d333dbbe772032586c0
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-12 19:41:34 +0000

    Add a cache

---
+++ src/cache.ts
@@ -0,0 +1,55 @@
+type CacheItem<T> = { timestamp: Date; item: T };
+
+interface Props {
+  timeoutSeconds: number;
+}
+
+const defaultProps: Props = {
+  timeoutSeconds: Number.MAX_SAFE_INTEGER,
+};
+
+function elapsedSeconds(date: Date): number {
+  return (new Date().getTime() - date.getTime()) / 1000;
+}
+
+export class Cache<T = any> {
+  private props: Props;
+  private cache = new Map<string, CacheItem<T>>();
+
+  constructor(props: Partial<Props> = defaultProps) {
+    this.props = { ...props, ...defaultProps };
+  }
+
+  async get(key: string): Promise<T | undefined> {
+    const { timestamp, item } = this.cache.get(key) ?? {
+      timestamp: new Date(0),
+    };
+    if (elapsedSeconds(timestamp) < this.props.timeoutSeconds) {
+      return item;
+    } else {
+      this.cache.delete(key);
+      return undefined;
+    }
+  }
+
+  async getWith(key: string, work: (key: string) => Promise<T>): Promise<T> {
+    const { timestamp, item } = this.cache.get(key) ?? {
+      timestamp: new Date(0),
+    };
+    if (elapsedSeconds(timestamp) < this.props.timeoutSeconds) {
+      return item!;
+    } else {
+      const found = await work(key);
+      this.set(key, found);
+      return found;
+    }
+  }
+
+  set(key: string, item: T) {
+    return this.cache.set(key, { timestamp: new Date(), item });
+  }
+
+  async fetch(url: string, key = url): Promise<T> {
+    return await this.getWith(key, () => fetch(url).then((x) => x.json()));
+  }
+}
