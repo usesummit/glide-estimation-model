3ed8ac8106e3c09e938fb01dc78cb338558a1c8e
David Siegel <dvdsgl@users.noreply.github.com>
2021-10-12 17:32:01 +0000

    Fix test

--- src/column.test.ts
+++ src/column.test.ts
@@ -1,6 +1,6 @@
 import main from "./column";
 
 test("We can get an echo", async () => {
-  const response = await main({ type: "string", value: "foo" });
+  const response = await main.run({ type: "string", value: "foo" });
   expect(response).toBe("echo foo");
 });
