705cd77fb9b2f63a695131311693e906d09d2463
Matt Wensing <matt@usesummit.com>
2024-04-02 05:43:58 -0500

    lets try filtering for a datapoint

--- src/column.ts
+++ src/column.ts
@@ -84,7 +84,7 @@ export default glide.column({
         body: JSON.stringify({
           "options": {
             "start": "2024-04-01",
-            "end": "2039-05-01",
+            "end": "2054-05-01",
             "resolution": "year"
           },
           "parameters": {
@@ -101,24 +101,23 @@ export default glide.column({
         })
       });
 
-      return JSON.stringify(modelData);
-
     } catch (error) {
       return JSON.stringify({'error': error})
     }
 
-    // // Filter results that have a 'values' property, then extract 'total_accrued_interest'
-    // const filteredResults = modelData.results.filter(r => r.values !== undefined);
-    // const lastResult = filteredResults[filteredResults.length - 1];
-    // let totalAccruedInterest: number;
+    // Filter results that have a 'values' property, then extract 'total_accrued_interest'
+    const filteredResults = modelData.results.filter(r => r.values !== undefined);
+    const lastResult = filteredResults[filteredResults.length - 1];
+    let totalAccruedInterest: number;
 
-    // if (lastResult && lastResult.values) {
-    //     totalAccruedInterest = Math.round(lastResult.values.total_accrued_interest);
-    // } else {
-    //     totalAccruedInterest = 0; // or handle this case as you see fit
-    // }
+    if (lastResult && lastResult.values) {
+        totalAccruedInterest = Math.round(lastResult.values.total_accrued_interest);
+    } else {
+        totalAccruedInterest = 0; // or handle this case as you see fit
+    }
 
-    // return JSON.stringify(modelData);
+    return JSON.stringify(modelData);
 
   },
+
 });
