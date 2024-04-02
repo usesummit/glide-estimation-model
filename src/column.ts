aa7ffe74f72bc150daeb67bca8aa5b5e770eeae6
Matt Wensing <matt@usesummit.com>
2024-04-02 14:00:15 -0500

    trying to return just the one we need

--- src/column.ts
+++ src/column.ts
@@ -51,8 +51,8 @@ export default glide.column({
         },
         body: JSON.stringify({
           "options": {
-              "start": "2024-02-01T00:00:00Z",
-              "end": "2025-03-01T00:00:00Z",
+              "start": "2024-04-01T00:00:00Z",
+              "end": "2025-04-30T00:00:00Z",
           },
           "parameters": {
               "ticket_submitted_at": submittedAt.value,
@@ -68,21 +68,21 @@ export default glide.column({
       return JSON.stringify({'error': error})
     }
 
-    // let completionAt;
+    let result;
 
-    // for (let i = modelData.results.length - 1; i >= 0; i--) {
-    //   const result = modelData.results[i];
-    //   // Check if 'completion_at' exists in the 'values' object
-    //   if (result.values && 'completion_at' in result.values) {
-    //     completionAt = result.values.completion_at;
-    //   }
-    // }
+    for (let i = modelData.results.length - 1; i >= 0; i--) {
+      result = modelData.results[i];      
+      // Check if 'completion_at' exists in the 'values' object
+      if (result.values && 'completion_at' in result.values) {
+        break;
+      }
+    }
 
     // // Filter results that have a 'values' property that has contents
     // const filteredResults = modelData.results.filter(r => r.values && Object.entries(r.values).length > 0);
     // const lastResult = filteredResults[filteredResults.length - 1];
 
-    return JSON.stringify({'data': modelData});
+    return JSON.stringify({'data': result});
 
   },
 
